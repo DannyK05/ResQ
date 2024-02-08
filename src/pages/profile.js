45adda3b92d1c981868a4f46d028563165c40693
Kolade <105021997+DannyK05@users.noreply.github.com>
2024-02-08 15:51:35 -0800

    Update profile.js

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -25,16 +25,16 @@ const Profile =() =>{
             <section className="mt-[10px]">
             <h1>Update your Info</h1>
             <form>
-            <input className="border-[1px] border-[#E7DDDD] p-[5px] hover:border-[#2592F6]  m-[10px] w-[40%] "  type="text" name="firstname"  placeholder="response.data.firstame" required/>
-                <input className="border-[1px] border-[#E7DDDD] p-[5px] hover:border-[#2592F6]  m-[10px] w-[40%] " type="text" name="lastname"  placeholder="response.data.lastname" required/>
-                <input className="border-[1px] border-[#E7DDDD] p-[5px] hover:border-[#2592F6]  m-[10px] w-[80%] " type="email" name="email"  placeholder="response.data.email" required/>
-                <input className="border-[1px] border-[#E7DDDD] p-[5px] hover:border-[#2592F6]  m-[10px] w-[80%] " type="number" name="phonenumber"  placeholder="response.data.phonenumber" required/> 
+            <input className="border-[1px] border-[#E7DDDD] p-[5px] hover:border-[#2592F6] rounded-[8px]  m-[10px] w-[40%] "  type="text" name="firstname"  placeholder={response.data.firstame} required/>
+                <input className="border-[1px] border-[#E7DDDD] p-[5px] hover:border-[#2592F6] rounded-[8px]  m-[10px] w-[40%] " type="text" name="lastname"  placeholder={response.data.lastname} required/>
+                <input className="border-[1px] border-[#E7DDDD] p-[5px] hover:border-[#2592F6] rounded-[8px]  m-[10px] w-[80%] " type="email" name="email"  placeholder={response.data.email} required/>
+                <input className="border-[1px] border-[#E7DDDD] p-[5px] hover:border-[#2592F6] rounded-[8px]  m-[10px] w-[80%] " type="number" name="phonenumber"  placeholder={response.data.phonenumber} required/> 
                 <br/>
                 <button className=" form__button border-neutral-400 rounded-xl bg-blue mx-[5px] my-[15px] px-[50px] py-[5px] w-[95%] text-white" type="submit"><h1>Update Info</h1></button>
             </form>
            
         </section>
-    )
+    
         </HomeLayout>
 
         
