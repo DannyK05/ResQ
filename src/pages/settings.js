cd8ca79caa70f9997163ba4d7faf0a020d0ac168
Kolade <105021997+DannyK05@users.noreply.github.com>
2024-02-08 16:16:12 -0800

    Update settings.js

--- src/pages/settings.js
+++ src/pages/settings.js
@@ -10,9 +10,9 @@ const Settings =() =>{
 
     return(
         <ActionLayout name ="Settings">
-            <p className="border-b-[1px] border-b-[#D9D9D9] p-[10px] text-black"><ion-icon className="px-[3px]" name="heart"></ion-icon>Manage your Medical Info <ion-icon name="arrow-forward-outline"></ion-icon></p>
-            <p className="border-b-[1px] border-b-[#D9D9D9] p-[10px] text-black"><ion-icon className="px-[3px]" name="medkit"></ion-icon>Manage your Personal Health Centre Info <ion-icon name="arrow-forward-outline"></ion-icon></p>
-            <p className="border-b-[1px] border-b-[#D9D9D9] p-[10px] text-black"><ion-icon className="px-[3px]" name="people"></ion-icon>Manage Special Ones Contact Info <ion-icon name="arrow-forward-outline"></ion-icon></p>
+            <p className="border-b-[1px] border-b-[#D9D9D9] p-[10px] text-black"><ion-icon className="mx-[3px]" size= "large" name="heart"></ion-icon>Manage your Medical Info <ion-icon name="arrow-forward-outline"></ion-icon></p>
+            <p className="border-b-[1px] border-b-[#D9D9D9] p-[10px] text-black"><ion-icon className="mx-[3px]" size= "large" name="medkit"></ion-icon>Manage your Personal Health Centre Info <ion-icon name="arrow-forward-outline"></ion-icon></p>
+            <p className="border-b-[1px] border-b-[#D9D9D9] p-[10px] text-black"><ion-icon className="mx-[3px]" size= "large" name="people"></ion-icon>Manage Special Ones Contact Info <ion-icon name="arrow-forward-outline"></ion-icon></p>
             <p onClick = {toggleVisibility} className="border-b-[1px] border-b-[#D9D9D9] p-[10px] text-black"><ion-icon className="px-[3px]" name="lock-closed"></ion-icon>Manage Password <ion-icon name="arrow-forward-outline"></ion-icon></p>
             <ModPassword visibility = {isVisible} Close ={toggleVisibility}/>
         </ActionLayout>
