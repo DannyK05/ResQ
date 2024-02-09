98f464277f337e186ce3a3ef6dbc26e2680f1253
Kolade <105021997+DannyK05@users.noreply.github.com>
2024-02-08 16:18:43 -0800

    Update notification.js

--- src/components/notification.js
+++ src/components/notification.js
@@ -3,7 +3,8 @@ import ActionLayout from "../containers/actionLayout"
 const Notification = () => {
     return(
         <ActionLayout name ="Notification">
-            <p className="text-blue text-[36px] mx-[5px] mt-[40%]"> No notification yet</p>
+        <ion-icon className="text-blue text-[24px]" name="notifications-off-outline"></ion-icon>
+            <p className="text-blue text-[24px] mx-[5px] mt-[40%]"> No notifications yet</p>
         </ActionLayout>
     )
 }
