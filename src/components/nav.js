7320d34807005b8ae7eb0071e528ee40d0146766
Kolade <105021997+DannyK05@users.noreply.github.com>
2024-02-09 00:30:49 -0800

    Update nav.js

--- src/components/nav.js
+++ src/components/nav.js
@@ -6,12 +6,12 @@ import ActiveProfile from "../assets/icons/MaleUser.svg";
 import Bluebtn from "../assets/icons/Group6.svg";
 import { Link } from 'react-router-dom';
 
-const Navbar = ({ toggleEmergencyTab }) => {
+const Navbar = ({ toggleEmergencyTab, visibility}) => {
     return(
         <nav className="fixed w-[100%] bottom-0">
             <ul className="flex w-[100%] justify-between align-center p-[5px]">
                <Link to="/home"> <li><img className="" alt="home" src={ActiveHome}/></li></Link>
-                <li><img className="" alt="Emergency button" onClick={toggleEmergencyTab} src={Bluebtn}/></li>
+                <li><img className="" alt="Emergency button" onClick={toggleEmergencyTab} src={visibility ? Bluebtn : Close}/></li>
                <Link to="/profile"><li><img className="" alt="profile" src={Profile}/></li></Link>
             </ul>
         </nav>
