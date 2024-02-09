236ce61ea87699d267ea223c0108bb750983226b
Kolade <105021997+DannyK05@users.noreply.github.com>
2024-02-09 14:08:14 -0800

    Update profile.js

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -3,20 +3,32 @@ import HomeLayout from "../containers/homeLayout";
 import {useEffect} from "react";
 
 const Profile =() =>{
+    const [userData, setUserData] = useState({
+    firstname: "",
+    lastname: "",
+    email: "",
+    phonenumber: "",
+  });
     
 // To fetch the current user data
-    const fetchUserData = async () =>{
-        try{
-            const response = await axios.get("https://resq-api-5j6r.onrender.com/api/v1/resQ/users/auth/signup");
-        }
-        catch (err){
-            console.log("There was an error getting data", err)
-        }
+  const fetchUserData = async () => {
+    try {
+      const response = await fetch("https://resq-api-5j6r.onrender.com/api/v1/resQ/users/auth/signup");
+      if (response.ok) {
+        const data = await response.json();
+        setUserData(data);
+      } else {
+        console.error("Error fetching data:", response.statusText);
+      }
+    } catch (err) {
+      console.error("There was an error getting data", err);
     }
-    // Call fetchUserData when the component mounts
-    useEffect(() => {
-        fetchUserData();
-    }, []);
+  };
+
+  // Call fetchUserData when the component mounts
+  useEffect(() => {
+    fetchUserData();
+  }, []);
     
     return(
         <HomeLayout>
@@ -26,10 +38,10 @@ const Profile =() =>{
             <section className="mt-[10px]">
             <h1>Update your Info</h1>
             <form>
-            <input className="border-[1px] border-[#E7DDDD] p-[5px] hover:border-[#2592F6] rounded-[8px]  m-[10px] w-[40%] "  type="text" name="firstname"  placeholder="response.data.firstame" required/>
-                <input className="border-[1px] border-[#E7DDDD] p-[5px] hover:border-[#2592F6] rounded-[8px]  m-[10px] w-[40%] " type="text" name="lastname"  placeholder="response.data.lastname" required/>
-                <input className="border-[1px] border-[#E7DDDD] p-[5px] hover:border-[#2592F6] rounded-[8px]  m-[10px] w-[80%] " type="email" name="email"  placeholder="response.data.email" required/>
-                <input className="border-[1px] border-[#E7DDDD] p-[5px] hover:border-[#2592F6] rounded-[8px]  m-[10px] w-[80%] " type="number" name="phonenumber"  placeholder="response.data.phonenumber" required/> 
+            <input className="border-[1px] border-[#E7DDDD] p-[5px] hover:border-[#2592F6] rounded-[8px]  m-[10px] w-[40%] "  type="text" name="firstname"  placeholder={userData.firstame} required/>
+                <input className="border-[1px] border-[#E7DDDD] p-[5px] hover:border-[#2592F6] rounded-[8px]  m-[10px] w-[40%] " type="text" name="lastname"  placeholder={userData.lastname} required/>
+                <input className="border-[1px] border-[#E7DDDD] p-[5px] hover:border-[#2592F6] rounded-[8px]  m-[10px] w-[80%] " type="email" name="email"  placeholder={userData.email} required/>
+                <input className="border-[1px] border-[#E7DDDD] p-[5px] hover:border-[#2592F6] rounded-[8px]  m-[10px] w-[80%] " type="tel" name="phonenumber"  placeholder={userData.phonenumber} required/> 
                 <br/>
                 <button className=" form__button border-neutral-400 active:bg-white active:text-blue rounded-xl bg-blue mx-[5px] my-[15px] px-[50px] py-[5px] w-[95%] text-white" type="submit"><h1>Update Info</h1></button>
             </form>
