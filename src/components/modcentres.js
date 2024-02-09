c796e562036edace572523b4f1ddff63c4ce30a3
Kolade <105021997+DannyK05@users.noreply.github.com>
2024-02-09 01:07:06 -0800

    Update modcentres.js

--- src/components/modcentres.js
+++ src/components/modcentres.js
@@ -1,11 +1,21 @@
+import {useState} from "react";
 
 const ModCentres = ({visibility, Close}) => {
+ const[centreFormVisibility, setFormVisibility] = useState(false)
+    const toggleFormVisibility = () =>{
+        setFormVisibility(!centreFormVisibility)
+    }
     return(
         <section className={`${visibility ? 'absolute' : 'hidden'} w-[90vw] bg-white py-[10px] px-[15px] rounded-[20px] z-10 shadow shadow-black top-[30%] left-[5vw]`}>           
-           <h1>Manage your password <ion-icon onClick={Close} size = "large" name="close-circle"></ion-icon></h1>
-            <form>
-                <input className="m-[8px] p-[4px] w-[80%]  border-[1px] border-[#E7DDDD] rounded-[6px]" type="password" placeholder="Input new password"/>
-                <input className="m-[8px] p-[4px] w-[80%]  border-[1px] border-[#E7DDDD] rounded-[6px]" type="password" placeholder="Confirm new password"/>
+           <h1>Manage your personal health centres <ion-icon onClick={Close} size = "large" name="close-circle"></ion-icon></h1>
+            <p>No health centres was added</p>
+             <button onClick={toggleFormVisibilty} className=" fixed bottom-[10px] right-[10px]]border-neutral-400 rounded-[100%]  text-white">
+                    <ion-icon size="large" name="add-circle"></ion-icon>
+                </button>
+            <form className ={`${centreFormVisibility ? hidden : block}`}>
+                <input className="m-[8px] p-[4px] w-[80%]  border-[1px] border-[#E7DDDD] rounded-[6px]" type="text" placeholder="Hospital name"/>
+                <input className="m-[8px] p-[4px] w-[80%]  border-[1px] border-[#E7DDDD] rounded-[6px]" type="number" placeholder="Contact number"/>
+                <input className="m-[8px] p-[4px] w-[80%]  border-[1px] border-[#E7DDDD] rounded-[6px]" type="email" placeholder="Contact mail"/>
                 <button className=" form__button border-neutral-400 rounded-xl bg-blue mx-[5px] my-[15px] px-[50px] py-[5px] w-[95%] text-white" type="submit">
                     <h1>Update</h1>
                 </button>
@@ -13,3 +23,4 @@ const ModCentres = ({visibility, Close}) => {
         </section>
     )
 }
+ export default ModCentres
