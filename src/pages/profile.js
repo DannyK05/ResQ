0f78410fbfab6776f0eda864f88143d52046b64d
Kolade <105021997+DannyK05@users.noreply.github.com>
2024-02-09 14:11:14 -0800

    Update profile.js

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -38,10 +38,10 @@ const Profile =() =>{
             <section className="mt-[10px]">
             <h1>Update your Info</h1>
             <form>
-            <input className="border-[1px] border-[#E7DDDD] p-[5px] hover:border-[#2592F6] rounded-[8px]  m-[10px] w-[40%] "  type="text" name="firstname"  placeholder={userData.firstame} required/>
-                <input className="border-[1px] border-[#E7DDDD] p-[5px] hover:border-[#2592F6] rounded-[8px]  m-[10px] w-[40%] " type="text" name="lastname"  placeholder={userData.lastname} required/>
-                <input className="border-[1px] border-[#E7DDDD] p-[5px] hover:border-[#2592F6] rounded-[8px]  m-[10px] w-[80%] " type="email" name="email"  placeholder={userData.email} required/>
-                <input className="border-[1px] border-[#E7DDDD] p-[5px] hover:border-[#2592F6] rounded-[8px]  m-[10px] w-[80%] " type="tel" name="phonenumber"  placeholder={userData.phonenumber} required/> 
+            <input className="border-[1px] border-[#E7DDDD] p-[5px] hover:border-[#2592F6] rounded-[8px]  m-[10px] w-[40%] "  type="text" name="firstname"  placeholder="userData.firstame" required/>
+                <input className="border-[1px] border-[#E7DDDD] p-[5px] hover:border-[#2592F6] rounded-[8px]  m-[10px] w-[40%] " type="text" name="lastname"  placeholder="userData.lastname" required/>
+                <input className="border-[1px] border-[#E7DDDD] p-[5px] hover:border-[#2592F6] rounded-[8px]  m-[10px] w-[80%] " type="email" name="email"  placeholder="userData.email" required/>
+                <input className="border-[1px] border-[#E7DDDD] p-[5px] hover:border-[#2592F6] rounded-[8px]  m-[10px] w-[80%] " type="tel" name="phonenumber"  placeholder="userData.phonenumber" required/> 
                 <br/>
                 <button className=" form__button border-neutral-400 active:bg-white active:text-blue rounded-xl bg-blue mx-[5px] my-[15px] px-[50px] py-[5px] w-[95%] text-white" type="submit"><h1>Update Info</h1></button>
             </form>
