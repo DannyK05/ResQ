8e4fbbb63586b47de71e889eba09de2f3d1d3e34
Kolade <105021997+DannyK05@users.noreply.github.com>
2024-02-09 01:21:53 -0800

    Update modmedical.js

--- src/components/modmedical.js
+++ src/components/modmedical.js
@@ -2,10 +2,57 @@
 const ModMedical = ({visibility, Close}) => {
     return(
         <section className={`${visibility ? 'absolute' : 'hidden'} w-[90vw] bg-white py-[10px] px-[15px] rounded-[20px] z-10 shadow shadow-black top-[30%] left-[5vw]`}>           
-           <h1>Manage your password <ion-icon onClick={Close} size = "large" name="close-circle"></ion-icon></h1>
-            <form>
-                <input className="m-[8px] p-[4px] w-[80%]  border-[1px] border-[#E7DDDD] rounded-[6px]" type="password" placeholder="Input new password"/>
-                <input className="m-[8px] p-[4px] w-[80%]  border-[1px] border-[#E7DDDD] rounded-[6px]" type="password" placeholder="Confirm new password"/>
+           <h1>Manage your medical info <ion-icon onClick={Close} size = "large" name="close-circle"></ion-icon></h1>
+            <form className="overflow-scroll">
+                <div>
+                    <h1><span className="text-blue">Personal</span> Medical Info</h1>
+                <label>Gender</label>    
+                <select className="border-2 border-[#E7DDDD] p-[5px] hover:border-[#2592F6] m-[10px] w-[40%]"  name="gender" >
+                    <option value ="Male">Male</option>
+                    <option value ="Female"> Female</option>
+                </select>
+                <label>Blood Type</label>
+                <select className="border-2 border-[#E7DDDD] p-[5px] hover:border-[#2592F6] m-[10px] w-[20%]"  name="bloodtype">
+                    <option value="A+"> A+</option>
+                    <option value="A-">A-</option>
+                    <option value="B+"> B+</option>
+                    <option value="B-"> B- </option> 
+                    <option value="O+"> O+</option>
+                    <option value="O-"> O- </option>
+                    <option value="AB+"> AB-</option>
+                    <option value="AB-">AB+</option>  
+                </select>
+                <br/>
+                <label for="disability">Do you have any disability</label>
+                <input name="disability" className="ml-[8px]" type ="radio"  value="true"/> <label>Yes</label>
+                <input name = "disability" className="ml-[8px]" type ="radio" value="false"/><label>No</label>
+                </div>
+                   
+                <div>
+                <h1><span className="text-blue">Medical</span> History</h1>
+                    <label>Do you have any allergy</label>
+                <input name="allergy" className="ml-[8px]" type ="radio" value="Yes" /> <label>Yes</label>
+                <input name = "allergy" className="ml-[8px]" type ="radio" value="No"/><label>No</label>
+                <br/>
+                <label>If yes please specify</label><input className="border-2 border-[#E7DDDD] p-[5px] hover:border-[#2592F6] m-[10px] w-[80%]" name='allergen' type= "text"/>
+                <br/>
+                <label>Chronic medical history or conditions</label>
+                <select className="border-2 border-[#E7DDDD] p-[5px] hover:border-[#2592F6] m-[10px] w-[80%]" name="condition">
+                <option value="A+"> A+</option>
+                    <option value="A-">A-</option>
+                    <option value="B+"> B+</option>
+                    <option value="B-"> B- </option> 
+                    <option value="O+"> O+</option>
+                    <option value="O-"> O- </option>
+                    <option value="AB+"> AB-</option>
+                    <option value="AB-">AB+</option>   
+                </select>
+                </div>
+                <div>
+                    <h1><span className="text-blue">Health</span> Insurance</h1>
+                    <input className="border-2 border-[#E7DDDD] p-[5px] hover:border-[#2592F6] m-[10px] w-[80%]" name="healthinsurance" type ="text" placeholder="Health Insurance Provider"/>
+                    <input className="border-2 border-[#E7DDDD] p-[5px] hover:border-[#2592F6] m-[10px] w-[80%]" name="policynumber" type ="text" placeholder="Policy Number"/>
+                </div>
                 <button className=" form__button border-neutral-400 rounded-xl bg-blue mx-[5px] my-[15px] px-[50px] py-[5px] w-[95%] text-white" type="submit">
                     <h1>Update</h1>
                 </button>
