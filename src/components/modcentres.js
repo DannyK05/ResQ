e8820e962b369040cb011bc14eb8dc248347a469
Kolade <105021997+DannyK05@users.noreply.github.com>
2024-02-09 13:58:45 -0800

    Update modcentres.js

--- src/components/modcentres.js
+++ src/components/modcentres.js
@@ -15,7 +15,7 @@ const ModCentres = ({visibility, Close}) => {
             <form className ={`${centreFormVisibility ? 'relative' : 'hidden'}`}>
                 <h1> Add health centres </h1>
                 <input className="m-[8px] p-[4px] w-[80%]  border-[1px] border-[#E7DDDD] rounded-[6px]" type="text" placeholder="Hospital name"/>
-                <input className="m-[8px] p-[4px] w-[80%]  border-[1px] border-[#E7DDDD] rounded-[6px]" type="number" placeholder="Contact number"/>
+                <input className="m-[8px] p-[4px] w-[80%]  border-[1px] border-[#E7DDDD] rounded-[6px]" type="tel" placeholder="Contact number"/>
                 <input className="m-[8px] p-[4px] w-[80%]  border-[1px] border-[#E7DDDD] rounded-[6px]" type="email" placeholder="Contact mail"/>
                 <button className=" active:bg-white active:text-blue border-neutral-400 rounded-xl bg-blue mx-[5px] my-[15px] px-[50px] py-[5px] w-[95%] text-white" type="submit">
                     <h1>Update</h1>
