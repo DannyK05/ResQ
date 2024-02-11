b0d82fc09094f2173406abb829dd4f104f76938d
DannyK05 <olubanjok@gmail.com>
2024-02-11 16:19:44 +0100

    Added autoplay in carousel

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -15,15 +15,15 @@ const Carousel = () => {
       <Slider {...settings}>
         <div className='w-[100%] flex flex-col justify-center align-center'>
           <img src="https://via.placeholder.com/800x400" alt="Slide 1" />
-          <p>Ask ResQbot </p>
+          {/* <p>Ask ResQbot </p> */}
         </div>
         <div className='w-[100%] flex flex-col justify-center align-center'>
           <img src="https://via.placeholder.com/800x400" alt="Slide 2" />
-          <p>Giving us permisson to use your location helps us alert health centres where to find you </p>
+          {/* <p>Giving us permisson to use your location helps us alert health centres where to find you </p> */}
         </div>
         <div className='w-[100%] flex flex-col justify-center align-center'>
           <img src="https://via.placeholder.com/800x400" alt="Slide 3" />
-          <p>Set up your health centres and special ones contact</p>
+          {/* <p>Set up your health centres and special ones contact</p> */}
         </div>
       </Slider>
     </div>
