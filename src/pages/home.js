06ab34aa8ba9d8846a5be4d452451c4b45daf32f
DannyK05 <olubanjok@gmail.com>
2024-03-01 09:27:46 +0100

    fix: Made the location warning card higher

--- src/pages/home.js
+++ src/pages/home.js
@@ -25,8 +25,8 @@ const Home = () => {
             <h1>Welcome</h1>
             <Advice />
             <ActionBtn />
-            <div className={`${visibility ? '' : 'hidden'} absolute z-10 bg-white p-4 w-[90%] ml-4 shadow shadow-black  rounded-[20px]`}>
-                <img className="w-full my-2" src={Permission} alt="Turn on location" />
+            <div className={`${visibility ? '' : 'hidden'} absolute z-10 top-[40%] bg-white p-4 w-[90%] ml-4 shadow shadow-black  rounded-[20px]`}>
+                <img className="w-3/4 mx-4 my-2" src={Permission} alt="Turn on location" />
                 <h1 className="text-blue">Turn on permission and reload</h1>
                 <p>Your location info helps us function better</p>
                 <button onClick={handleButtonClick} className="active:bg-blue active:text-white border-blue rounded-xl bg-white border-1 border-blue w-full shadow shadow-black my-2  px-[20px] py-[10px]  text-blue">
